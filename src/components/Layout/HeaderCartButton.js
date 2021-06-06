2aef33e2943e34636e6dabe79430ff580ec09cec
Ben Clauser <benringia@gmail.com>
2021-06-06 17:38:34 +0800

    added cart provider reducer

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,20 @@
+import { useContext } from "react";
 import CartIcon from "../Cart/CartIcon";
-import cartStyle from './HeaderCartButton.module.css'
+import cartStyle from "./HeaderCartButton.module.css";
+import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
   return (
     <button className={cartStyle.button} onClick={props.onClick}>
       <span className={cartStyle.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={cartStyle.badge}>3</span>
+      <span className={cartStyle.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
